9ff45c6a8a5684149ec88cda0acba7213badeffd
Steve Larsen <slarsen-3@att.net>
2021-01-29 17:35:09 -0600

    updated exerciseSchema in exercise.model.js

--- backend/models/exercise.model.js
+++ backend/models/exercise.model.js
@@ -3,11 +3,13 @@ const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
 const exerciseSchema = new Schema({
-    exercise: {type: String, required: true},
+    set: {type: Number, required: false},
     repetitions: {type: Number, required: false},
+    exercise: {type: String, required: true},
     weight: {type: Number, required: false},
     duration: {type: Number, required: false},
     distance: {type: Number, required: false},
+    date: {type: Date, required: true}
 }, {
     timestamps: true,
 });
